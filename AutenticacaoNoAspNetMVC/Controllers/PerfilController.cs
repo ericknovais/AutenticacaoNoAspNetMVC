d3ad04df4fe0a0e688fa672cd55905da697320a9
Erick Novais da Hora - EHO <erick.hora>
2020-03-24 18:48:15 -0300

    Aula 20 - Processando a alteração de senha

--- AutenticacaoNoAspNetMVC/Controllers/PerfilController.cs
+++ AutenticacaoNoAspNetMVC/Controllers/PerfilController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using AutenticacaoNoAspNetMVC.Models;
+using AutenticacaoNoAspNetMVC.Utils;
+using AutenticacaoNoAspNetMVC.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +13,38 @@ namespace AutenticacaoNoAspNetMVC.Controllers
 {
     public class PerfilController : Controller
     {
+        UsuariosContext ctx = new UsuariosContext();
+
         [Authorize]
-        // GET: Perfil
         public ActionResult AlterarSenha()
         {
             return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult AlterarSenha(AlterarSenhaViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var identity = User.Identity as ClaimsIdentity;
+            var login = identity.Claims.FirstOrDefault(x => x.Type == "Login").Value;
+
+            var usuario = ctx.Usuarios.FirstOrDefault(x => x.Login == login);
+
+            if (Hash.GerarHash(viewModel.SenhaAtual) != usuario.Senha)
+            {
+                ModelState.AddModelError("SenhaAtual", "Senha Incorreta");
+                return View();
+            }
+            usuario.Senha = Hash.GerarHash(viewModel.NovaSenha);
+            ctx.Entry(usuario).State = EntityState.Modified;
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index", "Painel");
         }
     }
 }
