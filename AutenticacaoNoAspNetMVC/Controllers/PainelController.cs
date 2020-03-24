0d1a1d38799b8d7e96d06ecc1cd9b49967b07c60
Erick Novais da Hora - EHO <erick.hora>
2020-03-24 16:40:28 -0300

    Aula 13 -  Compreendendo a URL de retorno

---
+++ AutenticacaoNoAspNetMVC/Controllers/PainelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutenticacaoNoAspNetMVC.Controllers
+{
+    public class PainelController : Controller
+    {
+        [Authorize]
+        // GET: Painel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
