da645bb6910664f1c16151ebe36853e65be71080
Erick Novais da Hora - EHO <erick.hora>
2020-03-27 06:40:21 -0300

    Ajustes

--- AutenticacaoNoAspNetMVC/Filters/AutorizacaoTipo.cs
+++ AutenticacaoNoAspNetMVC/Filters/AutorizacaoTipo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutenticacaoNoAspNetMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,9 @@ namespace AutenticacaoNoAspNetMVC.Filters
 {
     public class AutorizacaoTipo : AuthorizeAttribute
     {
-        private AutorizacaoTipo[] tiposAutorizados;
+        private TipoUsuario[] tiposAutorizados;
 
-        public AutorizacaoTipo(AutorizacaoTipo[] tipoUsuariosAutorizados)
+        public AutorizacaoTipo(TipoUsuario[] tipoUsuariosAutorizados)
         {
             tiposAutorizados = tipoUsuariosAutorizados;
         }
