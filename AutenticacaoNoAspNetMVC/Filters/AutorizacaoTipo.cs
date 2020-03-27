9ebf0da0978cb29c3b8c8687f2fc1fc90066c16e
Erick Novais da Hora - EHO <erick.hora>
2020-03-27 06:32:22 -0300

    Aula 09 - Criando um filtro customizado

---
+++ AutenticacaoNoAspNetMVC/Filters/AutorizacaoTipo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutenticacaoNoAspNetMVC.Filters
+{
+    public class AutorizacaoTipo : AuthorizeAttribute
+    {
+        private AutorizacaoTipo[] tiposAutorizados;
+
+        public AutorizacaoTipo(AutorizacaoTipo[] tipoUsuariosAutorizados)
+        {
+            tiposAutorizados = tipoUsuariosAutorizados;
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            bool autorizado = tiposAutorizados.Any(t => filterContext.HttpContext.User.IsInRole(t.ToString()));
+
+            if (!autorizado)
+            {
+                filterContext.Result = new RedirectResult("Painel");
+            } 
+        }
+    }
+}
