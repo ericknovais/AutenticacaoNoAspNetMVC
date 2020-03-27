3357fab39dcfd949133b2967450f0503e1830caa
Erick Novais da Hora - EHO <erick.hora>
2020-03-27 06:17:13 -0300

    Aula 08 - Usando o atributo Authorize

---
+++ AutenticacaoNoAspNetMVC/Controllers/CadastroUsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutenticacaoNoAspNetMVC.Controllers
+{
+    public class CadastroUsuarioController : Controller
+    {
+        [Authorize(Roles = "Administrador")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
