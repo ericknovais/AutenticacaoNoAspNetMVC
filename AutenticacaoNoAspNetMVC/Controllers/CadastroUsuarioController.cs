62b196474aaecccadb7f35837c61863e78453705
Erick Novais da Hora - EHO <erick.hora>
2020-03-27 06:38:28 -0300

    Aula 10 - Usando o filtro customizado

--- AutenticacaoNoAspNetMVC/Controllers/CadastroUsuarioController.cs
+++ AutenticacaoNoAspNetMVC/Controllers/CadastroUsuarioController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutenticacaoNoAspNetMVC.Filters;
+using AutenticacaoNoAspNetMVC.Models;
 
 namespace AutenticacaoNoAspNetMVC.Controllers
 {
     public class CadastroUsuarioController : Controller
     {
-        [Authorize(Roles = "Administrador")]
+        [AutorizacaoTipo(new[] { TipoUsuario.Administrador })]
         public ActionResult Index()
         {
             return View();
